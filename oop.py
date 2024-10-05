ef7926ccf0b2b18a4cf2efcc15426d8e7e5c49b0
h-banerjee <harshbanerjee30@gmail.com>
2024-10-06 00:34:16 +0530

    2nd commit

--- oop.py
+++ oop.py
@@ -1,6 +1,17 @@
-itemm1 = "phone"
-item1_price = 10,000
-item_quantity  = 2
-item1_price_total = item1_price*item_quantity
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    
+    def total_price(self):
+        return self.price * self.quantity
 
-print(item1_price_total)
+# Create an instance of Product
+item1 = Product("phone", 10000, 2)
+
+# Calculate total price for the item
+item1_total_price = item1.total_price()
+
+# Print total price
+print(f"Total price for {item1.name}: {item1_total_price}")
